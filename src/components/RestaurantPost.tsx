5156b8dd92fede7bad0c03cdedd6b22321f8a971
Alex Pierirodov <pererodovsasha2362000@gmail.com>
2025-02-27 23:50:56 +0200

    /

--- src/components/RestaurantPost.tsx
+++ src/components/RestaurantPost.tsx
@@ -17,14 +17,20 @@ const RestaurantPost = ({
 }: Restaurant) => {
 
     const threeTimes = times.slice(0, 3);
-
+    const link = `/restaurants/${slug}`;
+    
     return (
-        <Link href={`/restaurants/${slug}`} className='p-[5px] md:p-2.5 xl:p-[14px] pb-5 hover:bg-white hover:rounded-md hover:shadow-md transition-all duration-300 max-h-fit'>
+        <article  className='p-[5px] md:p-2.5 xl:p-[14px] pb-5 hover:bg-white hover:rounded-md hover:shadow-md transition-all duration-300 max-h-fit'>
             <div className="relative w-full min-h-[224px] mb-2.5 md:mb-[14px] overflow-hidden rounded-md">
-                <Image src={preview} alt={name} fill className="object-cover rounded-md hover:scale-110 transition-all duration-300" />
+                <Link href={link}>
+                    <Image src={preview} alt={name} fill className="object-cover rounded-md hover:scale-110 transition-all duration-300" />
+                </Link>
             </div>
             <div className="card-content">
-                <h2 className="text-base lg:text-lg font-bold text-tertiary mb-2.5 md:mb-5">{name}</h2>
+
+                <h2 className="text-base lg:text-lg font-bold text-tertiary mb-2.5 md:mb-5">
+                    <Link href={link}>{name}</Link>
+                </h2>
                 <p className="text-sm text-tertiary font-normal mb-[8px] md:mb-5" dangerouslySetInnerHTML={{ __html: location }} />
                 <p className="text-sm text-tertiary font-normal mb-2.5 md:mb-5"><strong>{timeframe.open}</strong> AM - <strong>{timeframe.close}</strong> PM</p>
                 <ul className="grid grid-cols-3 gap-2 lg:gap-5">
@@ -37,7 +43,7 @@ const RestaurantPost = ({
                     ))}
                 </ul>
             </div>
-        </Link>
+        </article>
     )
 }
 
--- src/components/restaurant/Slider.tsx
+++ src/components/restaurant/Slider.tsx
@@ -37,22 +37,22 @@ export default function Slider({ images }: { images: string[] }) {
             </Swiper>
 
             {/* Кнопка "Prev" */}
-            <div className="prev-button absolute left-4 top-1/2 -translate-y-1/2 z-20 cursor-pointer">
+            <button className="prev-button absolute left-4 top-1/2 -translate-y-1/2 z-20 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
                     <rect x="30" y="30" width="30" height="30" rx="15" transform="rotate(-180 30 30)" fill="white" fillOpacity="0.33" />
                     <path d="M21.3332 22.6117L21.3333 11.3884C21.3333 10.5326 20.3908 10.0126 19.6649 10.4784L10.8466 16.0901C10.6922 16.1878 10.565 16.3229 10.4769 16.483C10.3888 16.643 10.3426 16.8228 10.3426 17.0055C10.3426 17.1882 10.3888 17.3679 10.4769 17.528C10.565 17.688 10.6922 17.8232 10.8466 17.9209L19.6649 23.5217C19.8283 23.6274 20.0172 23.687 20.2117 23.6943C20.4061 23.7016 20.599 23.6562 20.7698 23.5631C20.9406 23.4699 21.0832 23.3323 21.1823 23.1649C21.2815 22.9974 21.3336 22.8063 21.3332 22.6117Z" fill="black" />
                     <path d="M22 20.355L20.4228 22L13.7114 15L20.4228 8L22 9.645L16.877 15L22 20.355ZM15.2886 20.355L13.7114 22L7 15L13.7114 8L15.2886 9.645L10.1655 15L15.2886 20.355Z" fill="white" />
                 </svg>
-            </div>
+            </button>
 
             {/* Кнопка "Next" */}
-            <div className="next-button absolute right-4 top-1/2 -translate-y-1/2 z-20 cursor-pointer" >
+            <button className="next-button absolute right-4 top-1/2 -translate-y-1/2 z-20 cursor-pointer" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" fill="none">
                     <rect width="30" height="30" rx="15" fill="white" fillOpacity="0.33" />
                     <path d="M8.66675 7.38827V18.6116C8.66675 19.4674 9.60925 19.9874 10.3351 19.5216L19.1534 13.9099C19.3078 13.8122 19.435 13.6771 19.5231 13.517C19.6112 13.357 19.6574 13.1772 19.6574 12.9945C19.6574 12.8118 19.6112 12.6321 19.5231 12.472C19.435 12.312 19.3078 12.1768 19.1534 12.0791L10.3351 6.47827C10.1717 6.37258 9.98279 6.31296 9.78832 6.30569C9.59386 6.29841 9.40103 6.34376 9.2302 6.43695C9.05936 6.53013 8.91684 6.6677 8.81767 6.83513C8.7185 7.00257 8.66636 7.19367 8.66675 7.38827Z" fill="black" />
                     <path d="M8 9.645L9.57718 8L16.2886 15L9.57718 22L8 20.355L13.123 15L8 9.645ZM14.7114 9.645L16.2886 8L23 15L16.2886 22L14.7114 20.355L19.8345 15L14.7114 9.645Z" fill="white" />
                 </svg>
-            </div>
+            </button>
         </div>
     )
 }
